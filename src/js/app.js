14c533e02bef216c642625682dcf44724fbc733a
tazocar <tankodani@gmail.com>
2018-02-18 17:57:17 -0300

    if diseño

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
 $(function() {
-  var socket = io('http://34.227.11.223:3000');
+  let socket = io('http://34.227.11.223:3000');
   $('form').submit(function() {
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
     return false;
   });
   socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
-    window.scrollTo(0, document.body.scrollHeight);
+    if (msg.length > 0) {
+      $('#messages').append($('<li>').text(msg));
+      window.scrollTo(0, document.body.scrollHeight);
+    }
   });
 });
